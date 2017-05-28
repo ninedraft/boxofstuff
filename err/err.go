f1f10edb7592f4f8d7a8d19fd63ed5c6a9574495
ninedraft <clamores.pro@gmail.com>
2017-05-28 12:06:09 +0300

    err main->err package name fix and Errorf args unpack fix

--- err/err.go
+++ err/err.go
@@ -1,13 +1,27 @@
-package main
+package err
 
 import (
 	"fmt"
 )
 
+// Err - string err
+// Usage:
+/*
+```
+import(
+	. "github.com/ninedraft/boxofstuff"
+)
+
+const(
+	ErrInvalidConfigFile Err = "invalid config file"
+)
+```
+*/
 type Err string
 
+// Errorf - creates Err from formatted string
 func Errorf(format string, args ...interface{}) Err {
-	return Err(fmt.Sprintf(format, args))
+	return Err(fmt.Sprintf(format, args...))
 }
 
 func (err Err) String() string {
